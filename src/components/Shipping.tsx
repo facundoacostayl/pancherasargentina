bf603d6c5e2b3dc1e06cf66a7252fffce5dd03ff
facundoacostayl <facundoacostayl@outlook.com>
2023-11-20 18:24:07 -0300

    Add guaranteecard to shipping component

--- src/components/Shipping.tsx
+++ src/components/Shipping.tsx
@@ -2,7 +2,7 @@ export const Shipping: React.FC = () => {
   return (
     <div>
       <h2 className="py-8 text-center text-gray-700 text-3xl font-bold">
-        - Comprá Tranquilo -
+        - Comprá con seguridad -
       </h2>
       <div className="w-full flex justify-between gap-5">
         <div className="rounded-md h-52 w-full relative background-">
@@ -15,7 +15,7 @@ export const Shipping: React.FC = () => {
             <h3 className="mb-3 text-white font-bold text-4xl">
               Compras seguras
             </h3>
-            <ul className="ml-1 text-left text-white text-lg font-medium">
+            <ul className="ml-4 text-left text-white text-lg font-medium">
               <li> - Aboná mediante MercadoPago</li>
               <li> - Tarjetas de débito y crédito</li>
               <li> - Efectivo en locales seleccionados</li>
@@ -26,7 +26,7 @@ export const Shipping: React.FC = () => {
           <img
             className="h-full w-full rounded-md"
             src="https://i.ibb.co/V9f6fd5/shippingcard-2.webp"
-            alt="mercadopagocard"
+            alt="shippingcard"
           />
           <div className="w-full text-center absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
             <h3 className="mb-3 text-white font-bold text-4xl">
@@ -37,7 +37,21 @@ export const Shipping: React.FC = () => {
             </h3>
           </div>
         </div>
-        <div className="border border-blue-400 h-52 w-full"></div>
+        <div className="rounded-md h-52 w-full relative background-">
+          <img
+            className="h-full w-full rounded-md"
+            src="https://i.ibb.co/n0m6Kwn/guaranteecard-1.webp"
+            alt="guaranteecard"
+          />
+          <div className="w-full text-center absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
+            <h3 className="mb-3 text-white font-bold text-4xl">
+              Garantía directa
+            </h3>
+            <h3 className="mb-3 text-white font-semibold text-xl">
+              con Pancheras Argentina
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
   );
