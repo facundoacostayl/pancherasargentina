d52ad056f9a761e09ca95f5bab130016e0a36e38
facundoacostayl <facundoacostayl@outlook.com>
2023-11-20 17:59:15 -0300

    Add shippingcard to shipping component

--- src/components/Shipping.tsx
+++ src/components/Shipping.tsx
@@ -12,16 +12,31 @@ export const Shipping: React.FC = () => {
             alt="mercadopagocard"
           />
           <div className="w-full text-center absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
-            <h3 className="mb-3 text-white font-bold text-3xl">
-              Compras seguras con MercadoPago
+            <h3 className="mb-3 text-white font-bold text-4xl">
+              Compras seguras
             </h3>
             <ul className="ml-1 text-left text-white text-lg font-medium">
+              <li> - Aboná mediante MercadoPago</li>
               <li> - Tarjetas de débito y crédito</li>
               <li> - Efectivo en locales seleccionados</li>
             </ul>
           </div>
         </div>
-        <div className="border border-blue-400 h-52 w-full"></div>
+        <div className="rounded-md h-52 w-full relative background-">
+          <img
+            className="h-full w-full rounded-md"
+            src="https://i.ibb.co/V9f6fd5/shippingcard-2.webp"
+            alt="mercadopagocard"
+          />
+          <div className="w-full text-center absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
+            <h3 className="mb-3 text-white font-bold text-4xl">
+              Envíos a todo el país
+            </h3>
+            <h3 className="mb-3 text-white font-semibold text-xl">
+              O retirá en nuestra fábrica en Castelar, GBA
+            </h3>
+          </div>
+        </div>
         <div className="border border-blue-400 h-52 w-full"></div>
       </div>
     </div>
