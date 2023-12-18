72b0ce9b0a8a4b1959faab805d0a8a4fa71169a4
facundoacostayl <facundoacostayl@outlook.com>
2023-12-18 01:04:37 -0300

    add ProductList to Shop page

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,128 @@
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+
+export const ProductList = () => {
+  return (
+    <div className="bg-white border">
+      <h2 className="pt-5 pb-3 ml-2 text-4xl font-semibold text-gray-500">
+        Nuestras Pancheras
+      </h2>
+      <div className="p-2">
+        <ul className="lg:grid lg:grid-cols-4 gap-2">
+          <li className="p-2 border">
+            <Link to="/">
+              <div className="text-center">
+                <div className="mx-5 mb-2 pb-4 border-b">
+                  <img
+                    src="https://i.ibb.co/SPSCpFJ/glpanchera5.webp"
+                    alt="product"
+                  />
+                  <h4 className="text-xl text-blue-900">
+                    Panchera SP16 Simple Electrica
+                  </h4>
+                </div>
+                <p className="text-2xl text-orange-500 font-medium">$5000,00</p>
+                <div className="inline-block border rounded-xl p-3 mt-2 mb-1 text-gray-800 hover:bg-gray-700 hover:text-white transition-all duration-200">
+                  <FontAwesomeIcon
+                    className="text-3xl"
+                    icon={faBagShopping}
+                  ></FontAwesomeIcon>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li className="p-2 border">
+            <Link to="/">
+              <div className="text-center">
+                <div className="mx-5 mb-2 pb-4 border-b">
+                  <img
+                    src="https://i.ibb.co/SPSCpFJ/glpanchera5.webp"
+                    alt="product"
+                  />
+                  <h4 className="text-xl text-blue-900">
+                    Panchera SP16 Simple Electrica
+                  </h4>
+                </div>
+                <p className="text-2xl text-orange-500 font-medium">$5000,00</p>
+                <div className="inline-block border rounded-xl p-3 mt-2 mb-1 text-gray-800 hover:bg-gray-700 hover:text-white transition-all duration-200">
+                  <FontAwesomeIcon
+                    className="text-3xl"
+                    icon={faBagShopping}
+                  ></FontAwesomeIcon>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li className="p-2 border">
+            <Link to="/">
+              <div className="text-center">
+                <div className="mx-5 mb-2 pb-4 border-b">
+                  <img
+                    src="https://i.ibb.co/SPSCpFJ/glpanchera5.webp"
+                    alt="product"
+                  />
+                  <h4 className="text-xl text-blue-900">
+                    Panchera SP16 Simple Electrica
+                  </h4>
+                </div>
+                <p className="text-2xl text-orange-500 font-medium">$5000,00</p>
+                <div className="inline-block border rounded-xl p-3 mt-2 mb-1 text-gray-800 hover:bg-gray-700 hover:text-white transition-all duration-200">
+                  <FontAwesomeIcon
+                    className="text-3xl"
+                    icon={faBagShopping}
+                  ></FontAwesomeIcon>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li className="p-2 border">
+            <Link to="/">
+              <div className="text-center">
+                <div className="mx-5 mb-2 pb-4 border-b">
+                  <img
+                    src="https://i.ibb.co/SPSCpFJ/glpanchera5.webp"
+                    alt="product"
+                  />
+                  <h4 className="text-xl text-blue-900">
+                    Panchera SP16 Simple Electrica
+                  </h4>
+                </div>
+                <p className="text-2xl text-orange-500 font-medium">$5000,00</p>
+                <div className="inline-block border rounded-xl p-3 mt-2 mb-1 text-gray-800 hover:bg-gray-700 hover:text-white transition-all duration-200">
+                  <FontAwesomeIcon
+                    className="text-3xl"
+                    icon={faBagShopping}
+                  ></FontAwesomeIcon>
+                </div>
+              </div>
+            </Link>
+          </li>
+          <li className="p-2 border">
+            <Link to="/">
+              <div className="text-center">
+                <div className="mx-5 mb-2 pb-4 border-b">
+                  <img
+                    src="https://i.ibb.co/SPSCpFJ/glpanchera5.webp"
+                    alt="product"
+                  />
+                  <h4 className="text-xl text-blue-900">
+                    Panchera SP16 Simple Electrica
+                  </h4>
+                </div>
+                <p className="text-2xl text-orange-500 font-medium">$5000,00</p>
+                <div className="inline-block border rounded-xl p-3 mt-2 mb-1 text-gray-800 hover:bg-gray-700 hover:text-white transition-all duration-200">
+                  <FontAwesomeIcon
+                    className="text-3xl"
+                    icon={faBagShopping}
+                  ></FontAwesomeIcon>
+                </div>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,6 +1,8 @@
 import { ShoppingCart } from "../components/ShoppingCart";
 import { Top } from "../components/Top";
+import { NavBar } from "../ui/NavBar";
 import { Footer } from "../components/Footer";
+import { ProductList } from "../components/ProductList";
 
 export const Shop = () => {
   return (
@@ -8,6 +10,8 @@ export const Shop = () => {
       <ShoppingCart></ShoppingCart>
       <div className="app">
         <Top></Top>
+        <NavBar></NavBar>
+        <ProductList></ProductList>
       </div>
       <Footer></Footer>
     </div>
