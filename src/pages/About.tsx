262121cd6ee4559695b90e9515eaa04676e1384d
facundoacostayl <facundoacostayl@outlook.com>
2024-01-03 10:30:48 -0300

    update about responsive

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,21 +13,21 @@ export const About = () => {
           <NavBar></NavBar>
         </div>
         <div className="py-12">
-          <div className="w-2/4 m-auto flex flex-col items-center justify-center text-center">
+          <div className="lg:w-2/4 m-auto flex flex-col items-center justify-center text-center">
             <p className="font-medium text-xl text-orange-500 mb-1">
               Sobre Nosotros
             </p>
             <h1 className="font-semibold text-6xl text-gray-700 mb-5">
               Nuestra Marca
             </h1>
-            <p className="font-light text-center text-gray-600 sm:text-xl px-44">
+            <p className="font-light text-center text-gray-600 sm:text-xl px-2 lg:px-44">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
               tenetur libero recusandae ipsam, illum quod exercitationem,
               tempora quos iste, quas eveniet. Libero reprehenderit consequatur
               inventore suscipit, possimus non assumenda eum!
             </p>
 
-            <div className="w-[700px] py-5">
+            <div className="lg:w-[700px] py-5">
               <img
                 className="object-fill"
                 src="https://i.ibb.co/C2fq9Tj/castelar.webp"
