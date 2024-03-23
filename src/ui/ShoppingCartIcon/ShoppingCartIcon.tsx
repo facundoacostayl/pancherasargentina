1115836f2c6928b906b38068e6a3a57c97feb864
facundoacostayl <facundoacostayl@outlook.com>
2024-03-23 08:16:10 -0300

    update shoppingCartIcon ui

--- src/ui/ShoppingCartIcon/ShoppingCartIcon.tsx
+++ src/ui/ShoppingCartIcon/ShoppingCartIcon.tsx
@@ -13,14 +13,14 @@ export const ShoppingCartIcon: React.FC = () => {
       className="w-[65px] h-[45px] cursor-pointer"
     >
       <div className="w-full h-full flex justify-center items-center rounded-md">
-        <div className="flex items-center justify-center w-full h-full bg-orange-500 rounded-l-md">
+        <div className="flex items-center justify-center w-full h-full bg-blue-950">
           <FontAwesomeIcon
             className="text-xl text-white"
             icon={faShoppingCart}
           ></FontAwesomeIcon>
         </div>
         {
-          <div className="w-3/4 h-full flex items-center justify-center border rounded-r-md ">
+          <div className="w-3/4 h-full flex items-center justify-center border border-blue-950">
             <p className="text-lg font-medium">
               {shoppingCartProductsQuantity}
             </p>
