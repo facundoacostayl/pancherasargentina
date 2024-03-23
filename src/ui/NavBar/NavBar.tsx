9958ba728b4d90629371031fa3a787ab1430e458
facundoacostayl <facundoacostayl@outlook.com>
2024-03-23 08:11:53 -0300

    update nav ui

--- src/ui/NavBar/NavBar.tsx
+++ src/ui/NavBar/NavBar.tsx
@@ -1,21 +1,53 @@
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faShop,
+  faPhone,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons";
+
 export const NavBar: React.FC = () => {
   return (
-    <nav className="hidden lg:block w-full z-50 p-2 bg-orange-500 rounded-t-md">
+    <nav className="hidden lg:block w-full z-50 p-2 bg-blue-950">
       <div className="md:w-[90%] lg:w-80% mx-auto flex justify-between items-center">
-        <ul className="w-full flex justify-between text-lg text-white font-semibold">
+        <ul className="w-full flex justify-between text-lg text-white font-medium">
           <li>
-            <Link to="/">Inicio</Link>
+            <Link
+              className="flex items-center gap-3 hover:text-gray-200 transition-all duration-100"
+              to="/"
+            >
+              <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
+              <p>Inicio</p>
+            </Link>
           </li>
           <li>
-            <Link to="/shop">Tienda</Link>
+            <Link
+              className="flex items-center gap-3 hover:text-gray-200 transition-all duration-100"
+              to="/shop"
+            >
+              <FontAwesomeIcon icon={faShop}></FontAwesomeIcon>
+              <p>Tienda</p>
+            </Link>
           </li>
           <li>
-            <Link to="/contacto">Contacto</Link>
+            <Link
+              className="flex items-center gap-3 hover:text-gray-200 transition-all duration-100"
+              to="/contacto"
+            >
+              <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>
+              <p>Contacto</p>
+            </Link>
           </li>
           <li>
-            <Link to="/sobre-nosotros">Sobre Nosotros</Link>
+            <Link
+              className="flex items-center gap-3 hover:text-gray-200 transition-all duration-100"
+              to="/sobre-nosotros"
+            >
+              <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
+              <p>Sobre Nosotros</p>
+            </Link>
           </li>
         </ul>
       </div>
